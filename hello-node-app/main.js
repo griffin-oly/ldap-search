6551218ae3b1027579cc25a759e6aef33e8de993
Ansible Service account <loc_ansible@CVYHJ3A21.oai.olympusglobal.com>
2018-06-04 13:22:33 -0400

    new logic for healthchecks

--- hello-node-app/main.js
+++ hello-node-app/main.js
@@ -1,12 +1,43 @@
-// load the http module
-var http = require('http');
+const http = require('http');
+const terminus = require('@godaddy/terminus');
 
-// configure our HTTP server
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Main Olympus Node Test\n");
-});
+function onSignal () {
+  console.log('server is starting cleanup');
+  return Promise.all([
+    // your clean logic, like closing database connections
+  ]);
+}
 
-// listen on localhost:8000
-server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+function onShutdown () {
+  console.log('cleanup finished, server is shutting down');
+}
+
+const server = http.createServer((request, response) => {
+  response.end(
+    `<html>
+      <body>
+        <h1>Hello, Olympus Griffin!</h1>
+       </body>
+     </html>`
+   );
+})
+
+const options = {
+  // healtcheck options
+  healthChecks: {
+    '/healthcheck': check          // a promise returning function indicating service health
+  },
+
+  // cleanup options
+  timeout: 1000,                   // [optional = 1000] number of milliseconds before forcefull exiting
+  signal,                          // [optional = 'SIGTERM'] what signal to listen for relative to shutdown
+  onSignal,                        // [optional] cleanup function, returning a promise (used to be onSigterm)
+  onShutdown,                      // [optional] called right before exiting
+
+  // both
+  logger                           // [optional] logger function to be called with errors
+};
+
+terminus(server, options);
+
+server.listen(PORT || 8000);
